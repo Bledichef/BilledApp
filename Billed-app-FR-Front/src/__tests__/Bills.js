1039bb57f70f4c952725efd39ff6c578501a1bdb
Guillaume C <81625005+Bledichef@users.noreply.github.com>
2023-05-30 09:02:54 +0200

    Ajout route_path, Route

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -7,7 +7,7 @@ import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
-import { ROUTES_PATH } from "../constants/routes.js";
+import { ROUTES_PATH, ROUTES } from "../constants/routes.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store";
 import Bills from "../containers/Bills.js";
@@ -46,5 +46,45 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
     });
+  });
+});
+
+describe("When I click on the new bill button", () => {
+  test("Then a modal with a form should open", () => {
+    //si l'user se trouve sur la page contenant bills dans url -> on affiche la route du même nom
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({ pathname });
+    };
+
+    const html = BillsUI({ data: bills });
+    document.body.innerHTML = html;
+
+    // On est sur la page employé
+    Object.defineProperty(window, "localStorage", { value: localStorageMock });
+    window.localStorage.setItem(
+      "user",
+      JSON.stringify({
+        type: "Employee",
+      })
+    );
+
+    // on instancie la classe Bills
+    const bill = new Bills({
+      document,
+      onNavigate,
+      store: null,
+      bills,
+      localStorage: window.localStorage,
+    });
+
+    const button = screen.getByTestId("btn-new-bill"); //on récup le data-testid du bouton
+
+    const handleClickNewBill = jest.fn(() => bill.handleClickNewBill());
+    button.addEventListener("click", handleClickNewBill); // au clic on appelle la fonction
+    userEvent.click(button); // On simule le click
+
+    expect(handleClickNewBill).toHaveBeenCalled(); //on vérifie que la fonction simulée a bien été appelée
+    const modaleNewBill = screen.getByTestId("form-new-bill"); // on récup le dataId de la modale
+    expect(modaleNewBill).toBeTruthy(); // on vérifie que la modale s'est bien ouverte (si elle apparait)
   });
 });
